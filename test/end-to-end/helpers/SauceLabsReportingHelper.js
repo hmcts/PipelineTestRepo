fc9ecba9a0180f846f959fec257b50e65958c5db
vakepati <37808224+vakepati@users.noreply.github.com>
2018-08-03 15:07:29 +0100

    Update SauceLabsReportingHelper.js

--- test/end-to-end/helpers/SauceLabsReportingHelper.js
+++ test/end-to-end/helpers/SauceLabsReportingHelper.js
@@ -5,8 +5,6 @@ let exec = require('child_process').exec;
 
 function updateSauceLabsResult(result, sessionId) {
   console.log('SauceOnDemandSessionID' + sessionId);
-  let SauceOnDemandSessionID = sessionId;
-  console.log('SaucOn Demand SessionId' + SauceOnDemandSessionID);
   return 'curl -X PUT -s -d \'{"passed": ' + result + '}\' -u ' + process.env.SAUCE_USERNAME + ':' + process.env.SAUCE_ACCESS_KEY + ' https://saucelabs.com/rest/v1/' + process.env.SAUCE_USERNAME + '/jobs/' + sessionId;
 }
 

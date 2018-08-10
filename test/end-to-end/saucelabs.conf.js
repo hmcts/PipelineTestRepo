f9dc867fd3b778279e64c468ae3e1a85bad424a6
vakepati <venkata.akepati@hmcts.net>
2018-08-10 15:25:25 +0100

    added proxy for sauce labs

--- test/end-to-end/saucelabs.conf.js
+++ test/end-to-end/saucelabs.conf.js
@@ -57,7 +57,7 @@ function getDesiredCapabilities() {
   let desiredCapability = supportedBrowsers[browser];
   desiredCapability.tunnelIdentifier = tunnelName;
   desiredCapability.tags = ['divorce'];
-  desiredCapability.proxy = 'proxyout.reform.hmcts.net:8080';
+  desiredCapability.httpProxy = 'proxyout.reform.hmcts.net:8080';
   return desiredCapability;
 }
 

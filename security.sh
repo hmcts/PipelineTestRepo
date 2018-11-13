2d4443938aae9cedd1e18c997c878fbacd7d452c
satya <1426759+satyachundur@users.noreply.github.com>
2018-11-13 16:10:03 +0000

    Update security.sh

--- security.sh
+++ security.sh
@@ -11,7 +11,7 @@ done
  echo "ZAP has successfully started"
  zap-cli --zap-url http://0.0.0.0 -p 1001 status -t 120
  zap-cli --zap-url http://0.0.0.0 -p 1001 open-url "${TEST_URL}"
- zap-cli context import ./security.context
+ zap-cli --zap-url http://0.0.0.0 -p 1001 context import ./security.context
  zap-cli --zap-url http://0.0.0.0 -p 1001 spider ${TEST_URL}
  zap-cli --zap-url http://0.0.0.0 -p 1001 active-scan --scanners all --recursive "${TEST_URL}"
  zap-cli --zap-url http://0.0.0.0 -p 1001 report -o activescan.html -f html

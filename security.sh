607ee2facaf0e9b98a9564b77f92ae88bac1ad34
satya <1426759+satyachundur@users.noreply.github.com>
2018-11-16 15:24:44 +0000

    Update security.sh

--- security.sh
+++ security.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "I have been executed from shell"
-echo "${SECURITYCONTEXT}" > ./security.context
+echo "${SECURITYCONTEXT}" > /root/.ZAP_D/contexts/security.context
 ls
 i=0
 while !(curl -s http://0.0.0.0:1001) > /dev/null
@@ -11,12 +11,12 @@ done
  echo "ZAP has successfully started"
  chmod +x security.context
  ls -la
- currentdirectory=${pwd}
+ currentdirectory=pwd()
  echo "current directory is ${currentdirectory}"
  
  zap-cli --zap-url http://0.0.0.0 -p 1001 status -t 120
  zap-cli --zap-url http://0.0.0.0 -p 1001 open-url "${TEST_URL}"
- zap-cli --zap-url http://0.0.0.0 context import "${currentdirectory}/security.context"
+ zap-cli --zap-url http://0.0.0.0 context import security.context
  zap-cli --zap-url http://0.0.0.0 -p 1001 spider ${TEST_URL}
  zap-cli --zap-url http://0.0.0.0 -p 1001 active-scan --scanners all --recursive "${TEST_URL}"
  zap-cli --zap-url http://0.0.0.0 -p 1001 report -o activescan.html -f html

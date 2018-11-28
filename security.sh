fc914d3510d117827346aee5f01f8b83260613a2
satya <1426759+satyachundur@users.noreply.github.com>
2018-11-28 16:31:31 +0000

    Update security.sh

--- security.sh
+++ security.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-echo "I have been executed from shell"
 echo "${SECURITYCONTEXT}" > /zap/security.context
-ls
-i=0
+if [ $UID -eq 0 ]; then
+  user=$1
+  dir=$2
+  cd "$dir"
+  exec su "zap" "$0" "$@"
+fi
+
 zap-x.sh -d -host 0.0.0.0 -port 1001 -config api.disablekey=true -config scanner.attackOnStart=true -config view.mode=attack -config connection.dnsTtlSuccessfulQueries=-1 -config api.addrs.addr.name=.* -config api.addrs.addr.regex=true /dev/null 2>&1 &
+i=0
 while !(curl -s http://0.0.0.0:1001) > /dev/null
 do
      i=$(( (i+1) %4 ))

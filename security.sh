93c67cae8fd9c6e4b21fa6547c02113fdd6521c2
satya <1426759+satyachundur@users.noreply.github.com>
2018-12-04 17:17:51 +0000

    Update security.sh

--- security.sh
+++ security.sh
@@ -14,7 +14,7 @@ done
  zap-cli --zap-url http://0.0.0.0 -p 1001 spider ${TEST_URL}
  zap-cli --zap-url http://0.0.0.0 -p 1001 active-scan --scanners all --recursive "${TEST_URL}"
  zap-cli --zap-url http://0.0.0.0 -p 1001 report -o activescan.html -f html
- su -m zap
+ su -s /bin/bash zap 
  echo $(whoami)
  zap-cli --zap-url http://0.0.0.0 -p 1001 ajax-spider ${TEST_URL}
  zap-cli --zap-url http://0.0.0.0 -p 1001 report -o ajaxspider.html -f html

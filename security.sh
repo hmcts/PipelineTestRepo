02e28c1f47f9468484b03429ac8b332a5d32ac8a
satya <1426759+satyachundur@users.noreply.github.com>
2019-09-25 16:56:54 +0100

    Update security.sh

--- security.sh
+++ security.sh
@@ -15,8 +15,8 @@ while !(curl -s http://0.0.0.0:1001) > /dev/null
   zap-cli --zap-url http://0.0.0.0 -p 1001 spider ${TEST_URL}
   zap-cli --zap-url http://0.0.0.0 -p 1001 active-scan -l Medium --scanners all --recursive "${TEST_URL}"
   zap-cli --zap-url http://0.0.0.0 -p 1001 report -o activescan.html -f html
-  zap-cli --zap-url http://0.0.0.0 -p 1001 ajax-spider ${TEST_URL}
-  zap-cli --zap-url http://0.0.0.0 -p 1001 report -o ajaxspider.html -f html
+  #zap-cli --zap-url http://0.0.0.0 -p 1001 ajax-spider ${TEST_URL}
+  #zap-cli --zap-url http://0.0.0.0 -p 1001 report -o ajaxspider.html -f html
   echo 'Changing owner from $(id -u):$(id -g) to $(id -u):$(id -u)'
   chown -R $(id -u):$(id -u) activescan.html
   curl --fail http://0.0.0.0:1001/OTHER/core/other/jsonreport/?formMethod=GET --output report.json
